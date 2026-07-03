499a3f45b817bae531cf3d6b8d4bfd9535daa7da
Incidious <jacobdock9@gmail.com>
2026-07-02 20:11:53 -0600

    Implement level-specific visibility for Bowser bombs
    Added checks to hide Bowser bombs in specific levels.

--- src/game/behaviors/bowser_bomb.inc.c
+++ src/game/behaviors/bowser_bomb.inc.c
@@ -1,6 +1,14 @@
 // bowser_bomb.c.inc
+#include "sm64ap.h"
 
 void bhv_bowser_bomb_loop(void) {
+    if ((gCurrLevelNum == LEVEL_BOWSER_1 && !sm64_have_bitdw_bombs)
+        || (gCurrLevelNum == LEVEL_BOWSER_2 && !sm64_have_bitfs_bombs)
+        || (gCurrLevelNum == LEVEL_BOWSER_3 && !sm64_have_bits_bombs)) {
+        cur_obj_hide();
+        cur_obj_become_intangible();
+        return;
+    }
     if (obj_check_if_collided_with_object(o, gMarioObject) == 1) {
         o->oInteractStatus &= ~INT_STATUS_INTERACTED;
         spawn_object(o, MODEL_EXPLOSION, bhvExplosion);
